ee7ef4a2f81dee707b8dffd67241636ef1a80d8b
DzmitryR <dzmitryreshko@gmail.com>
2022-08-15 04:44:13 +0300

    fixed code adaptive

--- src/components/newsCard/NewsCard.tsx
+++ src/components/newsCard/NewsCard.tsx
@@ -33,24 +33,24 @@ const NewsCard = () => {
                 <OpinionCard/>
             </div>
 
-                {
-                    newsDate.map(m => {
-                        return (
-                            <a href="/" className={s.newsCard} key={m.id}>
-                                <div className={s.newsCardImageBlock}>
-                                    <div className={s.newsCardImageWrapper}>
-                                        <img className={s.newsCardImage} src={m.image} alt={m.alt}/>
-                                    </div>
+            {
+                newsDate.map(m => {
+                    return (
+                        <a href="/" className={s.newsCard} key={m.id}>
+                            <div className={s.newsCardImageBlock}>
+                                <div className={s.newsCardImageWrapper}>
+                                    <img className={s.newsCardImage} src={m.image} alt={m.alt}/>
                                 </div>
+                            </div>
 
-                                <p className={s.newsCardData}>{m.data} | <span
-                                    className={s.newsCardHashTag}>#{m.hashTag}</span></p>
-                                <h5 className={s.newsCardTitle}>{m.title}</h5>
-                                <p className={s.newsCardDescription}>{m.description}</p>
-                            </a>
-                        )
-                    })
-                }
+                            <p className={s.newsCardData}>{m.data} | <span
+                                className={s.newsCardHashTag}>#{m.hashTag}</span></p>
+                            <h5 className={s.newsCardTitle}>{m.title}</h5>
+                            <p className={s.newsCardDescription}>{m.description}</p>
+                        </a>
+                    )
+                })
+            }
         </div>
     )
 
